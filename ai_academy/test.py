51742a778e13752b3aa6ca4e145577ed2bf4e4b3
Shin iwa <Luckystar0443@gmail.com>
2021-03-21 23:24:18 +0900

    refs 20210321001

---
+++ ai_academy/test.py
@@ -0,0 +1,22 @@
+a = 3
+print(a)
+b = 10
+print(b)
+print(a + b)
+price = 100
+tax = 0.08
+total_price = price * tax
+print(total_price)
+
+c = 10 + 3
+print(c)
+
+a1 = 10
+print(a1)
+print('hello')
+
+print(""""
+hello
+I'm Mike.
+Nice to meet you!
+""")
