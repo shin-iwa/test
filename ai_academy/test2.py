753daec864674b371fe566378e1d972d6a764550
Shin iwa <Luckystar0443@gmail.com>
2021-03-23 00:33:22 +0900

    refs #20210322001

--- ai_academy/test2.py
+++ ai_academy/test2.py
@@ -1,9 +1,20 @@
 ### 05 ###
 
-print(1 < 3)
-print(1 == 11-10)
-print(1 == 11-9)
-print(1 != 2)
-print(1 <= 2)
-a = 'Mike'
-print('Mike' == a)
+# print(1 < 3)
+# print(1 == 11-10)
+# print(1 == 11-9)
+# print(1 != 2)
+# print(1 <= 2)
+# a = 'Mike'
+# print('Mike' == a)
+
+### 06 ###
+
+a = 120
+print(a)
+print(type(a))
+print(type(100))
+b = 4.5
+print(type(b))
+complex = 3j
+print(type(complex))
